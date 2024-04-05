d87f562ffae171e7ac42a7f463b458c36362b339
Atharv Dalal <sangamdalal24@gmail.com>
2024-04-05 22:33:03 +0530

    Payment Verification✅

--- server/controllers/paymentController.js
+++ server/controllers/paymentController.js
@@ -1,5 +1,7 @@
 
 import { instance } from "../server.js"
+import CryptoJS from "crypto-js";
+import crypto from 'crypto'
 
 export const checkOut = async(req,res)=>{
   
@@ -17,3 +19,31 @@ const options = {
     order
  })
 }
+
+
+export const PaymentVerification = async (req, res) => {
+   const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+     req.body;
+ 
+   const body = razorpay_order_id + "|" + razorpay_payment_id;
+ 
+   const expectedSignature = crypto
+     .createHmac("sha256", process.env.RAZORPAY_API_SECRTE)
+     .update(body.toString())
+     .digest("hex");
+ 
+   const isAuthentic = expectedSignature === razorpay_signature;
+ 
+   if (isAuthentic) {
+     // Database comes here
+ 
+     res.redirect(
+       `http://localhost:5173/paymentsuccess?reference=${razorpay_payment_id}`
+     );
+   } else {
+     res.status(400).json({
+       success: false,
+     });
+   }
+ };
+
