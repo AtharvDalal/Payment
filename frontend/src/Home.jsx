47acae466efe4b76fd884543f8cff4f456b4cecc
Atharv Dalal <sangamdalal24@gmail.com>
2024-04-05 22:34:44 +0530

    Routes Added

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -4,14 +4,40 @@ import Card from './Card'
 import axios from 'axios'
 
 
-const Home = () => {
+
+ const Home = () => {
 
 
     const checkOutHandler = async(ammount)=>{
-        
-        const {} = await axios.post("http://localhost:8000",{
-  ammount
+         
+        const {data:{key}} = await axios.get("http://localhost:8000/api/getkey")
+
+        const {data: {order}} = await axios.post("http://localhost:8000/api/checkout",{
+            ammount
         })
+        const options = {
+            key,
+            amount: order.ammount , 
+            currency: "INR",
+            name: "Amit Patil",
+            description: "Buy Premium sub",
+            image: "https://e7.pngegg.com/pngimages/103/263/png-clipart-graphics-graph-premium-quality-logo-label-logo.png",
+            order_id: order.id , //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+            callback_url: "http://localhost:8000/api/payment" ,
+            prefill: {
+                name: "Gaurav Kumar",
+                email: "gaurav.kumar@example.com",
+                contact: "9325592220"
+            },
+            notes: {
+                "address": "Razorpay Corporate Office"
+            },
+            theme : {
+                "color": "#3399cc"
+            }
+        };
+        const razor = new window.Razorpay(options);
+            razor.open();
     }
   return (
      <Box>
@@ -29,5 +55,6 @@ const Home = () => {
 
   )
 }
+ 
 
 export default Home
--- server/routes/router.js
+++ server/routes/router.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { checkOut } from "../controllers/paymentController.js";
+import { checkOut, PaymentVerification } from "../controllers/paymentController.js";
 
 
 const paymentRouter = express.Router()
 
 
 paymentRouter.post('/checkout',checkOut)
+paymentRouter.post('/payment', PaymentVerification)
 
 export default paymentRouter
